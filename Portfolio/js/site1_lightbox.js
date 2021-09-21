51b336f47338cbb1863794107d244a907351d454
Rémi Lagardère <33605248+RemiLagardere@users.noreply.github.com>
2021-09-21 16:11:27 +0200

    Add files via upload

---
+++ Portfolio/js/site1_lightbox.js
@@ -0,0 +1,107 @@
+
+//lightbox 1
+
+var lightbox_link_1 = document.getElementById('site1_link');
+var lightbox_1 = document.getElementById('site1_lightbox');
+var exit_1 = document.getElementById('exit_1');
+
+lightbox_link_1.addEventListener('click', e => {
+        lightbox_1.classList.add('active');
+        document.body.style.overflow = 'hidden'
+
+})
+
+exit_1.addEventListener('click', e => {
+    lightbox_1.classList.remove('active');
+    document.body.style.overflow = 'scroll'
+})
+
+//lightbox 2
+
+var lightbox_link_2 = document.getElementById('site2_link');
+var lightbox_2 = document.getElementById('site2_lightbox');
+var exit_2 = document.getElementById('exit_2');
+
+lightbox_link_2.addEventListener('click', e => {
+        lightbox_2.classList.add('active');
+        document.body.style.overflow = 'hidden'
+
+})
+
+exit_2.addEventListener('click', e => {
+    lightbox_2.classList.remove('active');
+    document.body.style.overflow = 'scroll'
+})
+
+//lightbox 3
+
+var lightbox_link_3 = document.getElementById('site3_link');
+var lightbox_3 = document.getElementById('site3_lightbox');
+var exit_3 = document.getElementById('exit_3');
+
+lightbox_link_3.addEventListener('click', e => {
+        lightbox_3.classList.add('active');
+        document.body.style.overflow = 'hidden'
+
+})
+
+exit_3.addEventListener('click', e => {
+    lightbox_3.classList.remove('active');
+    document.body.style.overflow = 'scroll'
+})
+
+
+//lightbox 4
+
+var lightbox_link_4 = document.getElementById('site4_link');
+var lightbox_4 = document.getElementById('site4_lightbox');
+var exit_4 = document.getElementById('exit_4');
+
+lightbox_link_4.addEventListener('click', e => {
+        lightbox_4.classList.add('active');
+        document.body.style.overflow = 'hidden'
+
+})
+
+exit_4.addEventListener('click', e => {
+    lightbox_4.classList.remove('active');
+    document.body.style.overflow = 'scroll'
+})
+
+//lightbox 5
+
+var lightbox_link_5 = document.getElementById('site5_link');
+var lightbox_5 = document.getElementById('site5_lightbox');
+var exit_5 = document.getElementById('exit_5');
+
+lightbox_link_5.addEventListener('click', e => {
+        lightbox_5.classList.add('active');
+        document.body.style.overflow = 'hidden'
+
+})
+
+exit_5.addEventListener('click', e => {
+    lightbox_5.classList.remove('active');
+    document.body.style.overflow = 'scroll'
+})
+
+//lightbox 6
+
+var lightbox_link_6 = document.getElementById('site6_link');
+var lightbox_6 = document.getElementById('site6_lightbox');
+var exit_6 = document.getElementById('exit_6');
+
+lightbox_link_6.addEventListener('click', e => {
+        lightbox_6.classList.add('active');
+        document.body.style.overflow = 'hidden'
+
+})
+
+exit_6.addEventListener('click', e => {
+    lightbox_6.classList.remove('active');
+    document.body.style.overflow = 'scroll'
+})
+
+
+
+
